20e11ea2e7a0c6228b5347e35bdfbbef74eb284d
tusharpaudmal <123459049+tusharpaudmal@users.noreply.github.com>
2023-01-27 11:12:03 +0530

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,22 @@
+package com.ExampleGit.Model;
+public class Address{
+
+private int id;
+  private String localAddress;
+  
+  
+  public int getId() {
+	return id;
+}
+public void setId(int name) {
+	this.id = id;
+  
+  public String setLocalAddress(String localAddress)
+    this.localAddress=localAddress;
+  
+  public String getLocalAddress(){
+  return localAddress;
+  }
+
+
+}
