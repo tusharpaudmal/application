31571c8a6f873fadef134b6c610cee07fba9ef43
tusharpaudmal <123459049+tusharpaudmal@users.noreply.github.com>
2023-01-27 10:53:49 +0530

    this is first file on eclipse project

---
+++ DemoGitProject/src/com/ExampleGit/Model/StudentMain.java
@@ -0,0 +1,13 @@
+package com.ExampleGit.Model;
+
+public class StudentMain {
+	
+	public static void main(String[] args) {
+		Student student = new Student();
+		
+		System.out.println("student main class....");
+		
+		System.out.println("this is first example of git on eclipse ");
+	}
+
+}
