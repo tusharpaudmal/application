ea7c3cf58ae799b7447b18576484e2aac00be1b8
tusharpaudmal <123459049+tusharpaudmal@users.noreply.github.com>
2023-01-29 12:06:20 +0530

    Addition example changes....

---
+++ DemoGitProject/src/com/ExampleGit/Model/Addition.java
@@ -0,0 +1,17 @@
+package com.ExampleGit.Model;
+
+
+public class Addition {
+	public int a = 10;
+	public int b = 20;
+	public  int c = a + b;
+
+	public static void main(String[] args) {
+		Addition a= new Addition();
+		System.out.println("addition of a and b is :"+a.c);
+		System.out.println("this is car class....");
+		
+
+	}
+
+}
