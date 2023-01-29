72c96498f32d34ecfff3687faaca19f2f054ddcf
tusharpaudmal <123459049+tusharpaudmal@users.noreply.github.com>
2023-01-29 11:58:05 +0530

    Create Addition.java
    addition example

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition
+{
+  int a=10;
+  int b=20;
+  int c=a+b;
+public static void main(String [] args);
+  {
+  system.out.println("addition of a and b is :"+ c);
+  system.out.println("this is car class....");
+  
+  }
+
+}
